d7a0cdffbe46b11ef2b6914b8b6b415b3103d461
Matthew Bodily <mattcbodily@gmail.com>
2019-07-04 10:03:50 -0600

    boiler-plate code removed, menu component added

---
+++ src/Components/Menu/Menu.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import {MenuContainer} from './MenuStyles'
+
+const Menu = () => {
+    return(
+        <MenuContainer></MenuContainer>
+    )
+}
+
+export default Menu;
---
+++ src/Components/Menu/MenuStyles.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+export const MenuContainer = styled.div`
+    height: 100vh;
+    width: 6rem;
+    position: fixed;
+    left: 0;
+    background-color: #232729;
+`
