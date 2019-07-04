46c8053245cab2d34ffcc859130b424a7931e4eb
Matthew Bodily <mattcbodily@gmail.com>
2019-07-04 10:35:19 -0600

    initial styling for the menu component set-up

--- src/Components/Menu/Menu.js
+++ src/Components/Menu/Menu.js
@@ -1,9 +1,25 @@
 import React from 'react'
-import {MenuContainer} from './MenuStyles'
+import './Menu.css'
+import {MenuContainer, Img, P} from './MenuStyles'
+import logo from './../../assets/placeholder_logo.png'
+import {library} from '@fortawesome/fontawesome-svg-core'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faUsers, faCommentAlt, faTasks, faCalendarAlt} from '@fortawesome/free-solid-svg-icons'
+library.add(faUsers, faCommentAlt, faTasks, faCalendarAlt)
 
 const Menu = () => {
     return(
-        <MenuContainer></MenuContainer>
+        <MenuContainer>
+            <Img src={logo} alt='logo'/>
+            <FontAwesomeIcon icon='users' className='icons'/>
+            <P>Teams</P>
+            <FontAwesomeIcon icon='comment-alt' className='icons'/>
+            <P>Messages</P>
+            <FontAwesomeIcon icon='tasks' className='icons'/>
+            <P>Tasks</P>
+            <FontAwesomeIcon icon='calendar-alt' className='icons'/>
+            <P>Schedule</P>
+        </MenuContainer>
     )
 }
 
--- src/Components/Menu/MenuStyles.js
+++ src/Components/Menu/MenuStyles.js
@@ -2,8 +2,24 @@ import styled from 'styled-components'
 
 export const MenuContainer = styled.div`
     height: 100vh;
-    width: 6rem;
+    width: 5rem;
     position: fixed;
     left: 0;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    box-sizing: border-box;
+    padding-top: 15px;
     background-color: #232729;
+`
+
+export const Img = styled.img`
+    height: 55px;
+    margin-bottom: 25px;
+`
+
+export const P = styled.p`
+    color: #6C787D;
+    margin: 0px 0px 25px 0px;
+    font-size: 13px;
 `
